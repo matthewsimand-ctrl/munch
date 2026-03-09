a1f6dd185e0b5bdabcc82f034a9f0a8f8e397160
matthewsimand-ctrl <matthewsimand@matthews-mbp-2.home>
2026-03-08 20:34:03 -0400

    Snapshot of work before experiment

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.matt.munchchef',
+  appName: 'Munch',
+  webDir: 'dist'
+};
+
+export default config;
